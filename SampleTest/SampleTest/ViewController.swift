da2ef728a1432fca4a7cc8547b3da9c53d99df07
Maheswaran <maheswaran.c@cognizant.com>
2020-02-21 19:06:29 +0530

    Initial code

---
+++ SampleTest/SampleTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTest
+//
+//  Created by C, Maheswaran (Cognizant) on 21/02/20.
+//  Copyright © 2020 C, Maheswaran (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
